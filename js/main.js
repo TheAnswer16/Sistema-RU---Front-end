b8b9b30d1aba17dff9d30fd3af514cbb7d711b01
Alex Martins <64368305+TheAnswer16@users.noreply.github.com>
2022-04-11 11:24:27 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,84 @@
+const openNav = $('#open-nav');
+const closeNav = $('#close-nav');
+const openMode = $('#open-nav-mode');
+const nav = $('nav');
+const deleteBox = $('#container-delete');
+const deleteButton = $("#delete-button");
+const cancelButton = $("#cancel-button");
+const prevBox = $('#container-prev');
+const closePrev = $('#x-prev');
+
+nav.hide();
+
+deleteBox.hide();
+prevBox.hide();
+
+$('#prever').on("click", function(){
+    event.preventDefault();
+    openMode.fadeIn();
+    prevBox.fadeIn();
+});
+
+closePrev.on("click", function(){
+    openMode.fadeOut();
+    prevBox.fadeOut();
+});
+
+openNav.on("click", function(){
+    nav.animate({
+        width:'toggle'
+    }, 500);
+
+    $('nav ul li').fadeIn(1300);
+
+    openMode.fadeIn();
+});
+
+closeNav.on("click", function(){
+    nav.animate({
+        width:'toggle'
+    }, 500);
+
+    $('nav ul li').fadeOut(100);
+
+    openMode.fadeOut();
+});
+
+openMode.on("click", function(){
+    nav.animate({
+        width:'toggle'
+    }, 500);
+
+    $('nav ul li').fadeOut(100);
+
+    openMode.fadeOut();
+});
+
+$('.delete').on("click", function(){
+    openMode.fadeIn();
+    deleteBox.fadeIn();
+});
+
+deleteButton.on("click", function(){
+    openMode.fadeOut();
+    deleteBox.fadeOut();
+});
+
+cancelButton.on("click", function(){
+    openMode.fadeOut();
+    deleteBox.fadeOut();
+});
+
+
+
+const iconExpand = $('.icon-expand span');
+
+iconExpand.on("click", function(){
+    let id = $(this).attr('value');
+    $('.content-card-' + id).animate(
+        {
+            height:'toggle'
+        },500
+    );
+});
+
